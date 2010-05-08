723bb91047741c0323445da7e0d918e5d8026508
Alexander Baxevanis <alex.baxevanis@gmail.com>
2010-05-08 15:15:08 +0100

    Updated About screen and implemented app feedback feature

--- Classes/InfoViewController.m
+++ Classes/InfoViewController.m
@@ -8,17 +8,34 @@
 
 #import "InfoViewController.h"
 
-
 @implementation InfoViewController
+
+static NSString *appFeedbackEmailTemplate = 
+	@"Dear Cycle Hire App,<p>" \
+	"<i>Write about your suggestions or issues here</i><p>" \
+	"Thanks!";
 
 -(id) init {
 	if (self = [super init]) {
 		self.tableViewStyle = UITableViewStyleGrouped;
 		self.title = NSLocalizedString(@"About", nil);
 		
-		self.dataSource = [TTSectionedDataSource dataSourceWithObjects:@"",
-						   [TTTableTextItem itemWithText:NSLocalizedString(@"Acknowledgements", nil) URL:@"bundle://acknowledgements.html"],
-						   [TTTableTextItem itemWithText:NSLocalizedString(@"Feedback", nil) URL:@""],
+		TTURLMap *map = [TTNavigator navigator].URLMap;
+		[map from:@"cyclehire://information/feedback" toObject:self selector:@selector(appFeedback)];
+
+		
+		self.dataSource = [TTSectionedDataSource dataSourceWithObjects:@"Acknowledgements",
+						   [TTTableSubtitleItem itemWithText:@"Maps by OpenStreetMap" 
+													subtitle:@"Under a CC-BY-SA 2.0 license"
+													imageURL:@"bundle://cycle-hire-icon.png" 
+														 URL:@"http://www.openstreetmap.org/"],
+						   [TTTableSubtitleItem itemWithText:@"Directions by CycleStreets" 
+													subtitle:nil
+													imageURL:@"bundle://cycle-hire-icon.png" 
+														 URL:@"http://www.cyclestreets.net"],
+						   [TTTableTextItem itemWithText:NSLocalizedString(@"More ...", nil) URL:@"bundle://acknowledgements.html"],
+						   @"",
+						   [TTTableButton itemWithText:NSLocalizedString(@"Send us your feedback", nil) URL:@"cyclehire://information/feedback"],
 						   nil];
 	}
 	return self;
@@ -27,6 +44,39 @@
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
 	self.navigationController.navigationBarHidden = NO;
+}
+
+- (void) appFeedback {
+	MFMailComposeViewController *mailComposer = [[MFMailComposeViewController alloc] init];
+	mailComposer.mailComposeDelegate = self;
+	
+	[mailComposer setSubject:@"Feedback for Cycle Hire app"];
+	NSArray *toRecipients = [NSArray arrayWithObject:@"feedback@cyclehireapp.com"];
+	[mailComposer setToRecipients:toRecipients];
+	
+	NSString *emailBody = appFeedbackEmailTemplate;
+	[mailComposer setMessageBody:emailBody isHTML:YES];
+	
+	[self presentModalViewController:mailComposer animated:YES];
+	[mailComposer release];
+}
+
+
+- (void)mailComposeController:(MFMailComposeViewController *)controller
+		  didFinishWithResult:(MFMailComposeResult)result
+						error:(NSError *)error
+{
+    [self dismissModalViewControllerAnimated:YES];
+	
+	if (result == MFMailComposeResultSent) {
+		UIAlertView *thankYouAlert = [[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Thank you", nil) 
+																message:NSLocalizedString(@"We review all suggestions and issues and will reply to your email if appropriate.", nil)   
+															   delegate:nil 
+													  cancelButtonTitle:NSLocalizedString(@"OK", nil) 
+													  otherButtonTitles:nil];
+		[thankYouAlert show];
+		[thankYouAlert release];
+	}	
 }
 
 @end
